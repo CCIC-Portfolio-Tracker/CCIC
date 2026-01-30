843524a83041970f9ecabf7bc1f5e5134a54c0fe
james-treadwell <jamestreadwell626@gmail.com>
2026-01-29 17:36:30 -0700

    create database script

--- backend/src/all_database_create.js
+++ backend/src/all_database_create.js
@@ -1,30 +1,22 @@
-// runner.js
 import { createDatabase } from './database_creation.js';
 import { createTickerDatabase } from './ticker_database_creation.js';
 import { createPortfolioDatabase } from './portfolio_database_creation.js';
 import { createHoldingDatabase } from './brute_holding_database_creation.js';
 
-
-
-
 async function runEverything() {
     try {
-        console.log("--- Starting Daily Task Pipeline ---");
+        console.log("Creating Database");
 
-        console.log("1. Updating Holdings...");
         await createDatabase();
 
-        console.log("2. Backing up Database...");
         await createTickerDatabase();
 
-        console.log("3. Generating Reports...");
         await createPortfolioDatabase();
 
         await createHoldingDatabase();
 
-        console.log("--- All Tasks Completed Successfully ---");
     } catch (error) {
-        console.error("Pipeline failed at some step:", error);
+        console.error("Error creating database", error);
     }
 }
 
